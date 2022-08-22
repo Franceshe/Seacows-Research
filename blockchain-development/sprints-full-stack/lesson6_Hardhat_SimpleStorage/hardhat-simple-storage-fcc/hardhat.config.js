a96fd74834ec1e60ea4fb966447005eca795c20e
Frances He <hesiyun1996@gmail.com>
2022-08-23 03:54:00 +0800

    add: hardhat etherscan automatic verification

--- blockchain-development/sprints-full-stack/lesson6_Hardhat_SimpleStorage/hardhat-simple-storage-fcc/hardhat.config.js
+++ blockchain-development/sprints-full-stack/lesson6_Hardhat_SimpleStorage/hardhat-simple-storage-fcc/hardhat.config.js
@@ -1,11 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
 
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+
 module.exports = {
   //defaultNetwork: "hardhat": with automative private key and rpc url
   // better be specific
@@ -18,6 +21,9 @@ module.exports = {
     },
   },
   solidity: "0.8.8",
+  etherscan:{
+    apiKey: ETHERSCAN_API_KEY
+  }
 };
  
 //https://chainlist.org/
--- blockchain-development/sprints-full-stack/lesson6_Hardhat_SimpleStorage/hardhat-simple-storage-fcc/scripts/deploy.js
+++ blockchain-development/sprints-full-stack/lesson6_Hardhat_SimpleStorage/hardhat-simple-storage-fcc/scripts/deploy.js
@@ -1,6 +1,8 @@
 // imports
 //const {ethers} = require('ethers');
-const {ethers} = require("hardhat")
+// run allows us to run any hardhat tasks
+const {ethers, run} = require("hardhat")
+
 
 
 // async main 
@@ -12,6 +14,22 @@ async function main(){
   // what's the private key?
   // what's the rpc url?
   console.log(`Deployed contract to ${simpleStorage.address}`)
+  //Deployed contract to 0x5FbDB2315678afecb367f032d93F642f64180aa3
+
+}
+
+  //automatic verify
+async function verify(contractAddress, args){
+  // works on etherscan -> or do it with explore api
+  // verify contract programmatically
+  // hardhat plugin! hardhat-etherscan plugin :)
+  console.log("Verifying contract...")
+  await run("verify:verify", {
+    address: contractAddress,
+    constructorArgs: args,
+  })
+
+
 }
 
 // main
@@ -19,3 +37,9 @@ main().then(() => process.exit(0)).catch((error) =>{
   console.error(error);
   process.exit(1);
 });
+
+// Network in hardhart
+//Deployed contract to 0x6CbFa96BB49c48428356d0d4366B6e3Bf74e2fAf
+// Prgamatic verification
+//`yarn add --dev @nomiclabs/hardhat-etherscan `
+// added: verify   Verifies contract on Etherscan 
