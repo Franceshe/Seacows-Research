c8349f51aa308ad024e9ecc083749b3fcaabc338
Frances He <hesiyun1996@gmail.com>
2022-08-24 00:32:24 +0800

    add: hardhat localhost node

--- blockchain-development/sprints-full-stack/lesson6_Hardhat_SimpleStorage/hardhat-simple-storage-fcc/hardhat.config.js
+++ blockchain-development/sprints-full-stack/lesson6_Hardhat_SimpleStorage/hardhat-simple-storage-fcc/hardhat.config.js
@@ -20,6 +20,11 @@ module.exports = {
       accounts: [PRIVATE_KEY], 
       chainId: 5,
     },
+    localhost:{
+      url: "http://127.0.0.1:8545/",
+      //accounts: thankds hardhat!
+      chainId: 31337,
+    }
   },
   solidity: "0.8.8", 
   etherscan:{
