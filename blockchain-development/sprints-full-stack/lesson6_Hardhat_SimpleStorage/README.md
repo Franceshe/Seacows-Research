ff834fa39f665ce03736663b95f377955dbc578c
Frances He <hesiyun1996@gmail.com>
2022-08-23 23:50:01 +0800

    add verify on testnet, failed at georli, check later

---
+++ blockchain-development/sprints-full-stack/lesson6_Hardhat_SimpleStorage/README.md
@@ -0,0 +1,17 @@
+## Lesson 6 Hardhat fundme
+
+### hardhat setup
+* yarn = npm
+* yarn = npx
+* yarn add --dev prettier prettier-plugin-solidity
+* deployed to specifc network: `yarn hardhat run scripts/deploy.js --network hardhat`
+* env: yarn add --dev dotenv
+* useful: `yarn hardhat run scripts/deploy.js --network goerli --show-stack-traces`
+
+
+## How to visualize smart contract 
+* ref: https://www.youtube.com/watch?v=Nv-dV69lIQw&ab_channel=EatTheBlocks
+
+## surya: for visulize solidity code
+*`surya graph contracts/**/*.sol | dot -Tpng > MyContract.png`
+
--- blockchain-development/sprints-full-stack/lesson6_Hardhat_SimpleStorage/hardhat-simple-storage-fcc/scripts/deploy.js
+++ blockchain-development/sprints-full-stack/lesson6_Hardhat_SimpleStorage/hardhat-simple-storage-fcc/scripts/deploy.js
@@ -1,7 +1,7 @@
 // imports
 //const {ethers} = require('ethers');
 // run allows us to run any hardhat tasks
-const {ethers, run} = require("hardhat")
+const {ethers, run, network} = require("hardhat")
 
 
 
@@ -16,20 +16,52 @@ async function main(){
   console.log(`Deployed contract to ${simpleStorage.address}`)
   //Deployed contract to 0x5FbDB2315678afecb367f032d93F642f64180aa3
 
+  // what happends when we deploy to our hardhat network?
+  // check network config
+  //console.log(network.config)
+
+  // verify on Testnet on georli
+  if(network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY){
+    console.log("Waiting for block txes...")
+    // give some time for etherscan to include it in block
+    await simpleStorage.deployTransaction.wait(6);
+    await verify(simpleStorage.address, [])
+  }
+
+  // interact with the contract
+  const currentValue = await simpleStorage.retrieve()
+  console.log(`Current value is: ${currentValue}`)
+
+  // Update the curent value
+  const transactionResponse = await simpleStorage.store(7)
+  await transactionResponse.wait(1)
+  const updatedValue = await simpleStorage.retrieve()
+  console.log(`Updated Value is :${updatedValue}`)
+
 }
 
   //automatic verify
+  // won't work on thihs verify func with local(hardhat) network
 async function verify(contractAddress, args){
   // works on etherscan -> or do it with explore api
   // verify contract programmatically
   // hardhat plugin! hardhat-etherscan plugin :)
   console.log("Verifying contract...")
+  // if contract already being verifyed
+  try{
   await run("verify:verify", {
     address: contractAddress,
-    constructorArgs: args,
+    constructorArguments: args,
   })
-
-
+  }catch (e){
+    if(e.message.toLowerCase().includes("already verified")){
+      console.log("Already Verified")
+    }
+    else{
+      console.log(e)
+    }
+  }
+  
 }
 
 // main
@@ -42,4 +74,5 @@ main().then(() => process.exit(0)).catch((error) =>{
 //Deployed contract to 0x6CbFa96BB49c48428356d0d4366B6e3Bf74e2fAf
 // Prgamatic verification
 //`yarn add --dev @nomiclabs/hardhat-etherscan `
-// added: verify   Verifies contract on Etherscan 
+// added: verify   Verifies contract on Etherscan
+// Interacting with contracts in hardhart
