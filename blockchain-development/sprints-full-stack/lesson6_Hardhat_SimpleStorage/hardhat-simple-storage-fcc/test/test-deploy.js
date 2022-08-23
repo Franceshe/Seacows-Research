43c7f04a0f0424f09199be76eda0c12b48d0be88
Frances He <hesiyun1996@gmail.com>
2022-08-24 01:00:37 +0800

    add: writing test

---
+++ blockchain-development/sprints-full-stack/lesson6_Hardhat_SimpleStorage/hardhat-simple-storage-fcc/test/test-deploy.js
@@ -0,0 +1,46 @@
+const {ethers} = require('hardhat')
+const {expect, assert} = require('chai')
+function testFunc(){
+
+
+}
+
+// best practice
+describe("SimpleStorage", function(){
+  //let simpleStorageFactory,
+  //let simpleStorage,
+  let simpleStorageFactory, simpleStorage
+
+  beforeEach(async function(){
+    simpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
+    simpleStorage = await simpleStorageFactory.deploy()
+  })
+
+  it("Should start with  favorite number of 0", async function(){
+    const currentValue = await simpleStorage.retrieve()
+    const expectedValue = "0"
+    // assert
+    // expect
+    assert.equal(currentValue.toString(), expectedValue)
+  })
+
+  //it.only
+  it("should update when we call store", async function(){
+    const expectedValue = "7"
+    const transactionResponse = await simpleStorage.store(expectedValue)
+    await transactionResponse.wait(1)
+ 
+    const currentValue = await simpleStorage.retrieve()
+    assert.equal(currentValue.toString(), expectedValue)
+     
+  })
+})
+  
+   
+
+// describe("SimpleStorage", () => {})
+// custom: make function anonymous 
+
+async function main(){
+  const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
+}
