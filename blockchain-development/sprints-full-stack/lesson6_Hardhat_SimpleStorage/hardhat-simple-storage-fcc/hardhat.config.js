858a155b9bb11d303f5431430218b58a0977fa37
Frances He <hesiyun1996@gmail.com>
2022-08-24 01:22:00 +0800

    env recap

--- blockchain-development/sprints-full-stack/lesson6_Hardhat_SimpleStorage/hardhat-simple-storage-fcc/hardhat.config.js
+++ blockchain-development/sprints-full-stack/lesson6_Hardhat_SimpleStorage/hardhat-simple-storage-fcc/hardhat.config.js
@@ -7,10 +7,15 @@ require("hardhat-gas-reporter")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL   
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+
+// const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "https://eth-goerli"
+// const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey"
+// const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+// const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
 
 module.exports = {
   //defaultNetwork: "hardhat": with automative private key and rpc url
@@ -33,11 +38,12 @@ module.exports = {
     apiKey: ETHERSCAN_API_KEY
   },
   gasReporter:{
-    enabled: true, 
+    enabled: false, 
     outputFile: "gas-report.txt",
     noColors: true,
     currency: "USD",
     coinMarketcap:COINMARKETCAP_API_KEY,
+    token: "MATIC",
   }
 
 };
