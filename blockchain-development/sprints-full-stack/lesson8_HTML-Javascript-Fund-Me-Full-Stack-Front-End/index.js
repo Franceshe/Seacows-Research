8650f91cb0984e6e49f9bcb0a0896716dbed7a02
Frances He <hesiyun1996@gmail.com>
2022-10-04 04:24:51 +0800

    fix fund button and abi import

--- blockchain-development/sprints-full-stack/lesson8_HTML-Javascript-Fund-Me-Full-Stack-Front-End/index.js
+++ blockchain-development/sprints-full-stack/lesson8_HTML-Javascript-Fund-Me-Full-Stack-Front-End/index.js
@@ -5,6 +5,7 @@
 // import
 //const {ethers} = require("ethers")
 import { ethers } from "./ethers-5.6.esm.min.js"
+//import { abi, contractAddress } from "./constants.js"
 import { abi, contractAddress } from "./constants.js"
 
 const connectButton = document.getElementById("connectButton")
@@ -14,25 +15,38 @@ fundButton.onclick = fund
 
 //console.log(ethers)
 
-async function connect() {
-  if (typeof window.ethereum != "undefined") {
-    await window.ethereum.request({ method: "eth_requestAccounts" })
-    // document.getElementById("connectButton").innerHTML = "Connected!"
-    connectButton.innerHTML = "connected"
+// async function connect() {
+//   if (typeof window.ethereum !== "undefined") {
+//     try {
+//       await window.ethereum.request({ method: "eth_requestAccounts" })
+//     } catch (error) {
+//       console.log(error)
+//     }
+//     connectButton.innerHTML = "connected"
+//   } else {
+//     connectButton.innerHTML = "Please install Metamask"
+//   }
+// }
 
-    //console.log("connected!")
+async function connect() {
+  if (typeof window.ethereum !== "undefined") {
+    try {
+      await ethereum.request({ method: "eth_requestAccounts" })
+    } catch (error) {
+      console.log(error)
+    }
+    connectButton.innerHTML = "Connected"
+    const accounts = await ethereum.request({ method: "eth_accounts" })
+    console.log(accounts)
   } else {
-    ///console.log("No metamask")
-    //document.getElementById("connectButton").innerHTML =
-    //;("Please install metamask!")
-    connectButton.innerHTML = "Please install Metamask"
+    connectButton.innerHTML = "Please install MetaMask"
   }
 }
 
 async function fund() {
   const ethAmount = "77"
   console.log(`Funding with ${ethAmount}...`)
-  if (typeof window.ethereum != "undefined") {
+  if (typeof window.ethereum !== "undefined") {
     //provider / connection to the blockchain
     //signer /wallet / someone with some gas to sent it
     // contract that we are interacting with
